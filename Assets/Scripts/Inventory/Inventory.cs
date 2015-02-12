053fb2849291933e8f20ab8e78648907ce9f3b9b
fabiusBile <dock234@yandex.ru>
2015-02-12 20:21:24 +0500

    Начало написание инвентаря
    Начало написание инвентаря

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.IO;
+
+public class Inventory : ScriptableObject {
+	List<Item> inventory;
+	XmlDocument invDoc;
+	void Awake(){
+		inventory = new List<Item>();
+	}
+	void Add (Item item){
+
+	}
+	List<Item> Load(){
+		invDoc = new XmlDocument();
+		try {
+			invDoc.Load ("Data/inventory.xml");
+		} catch (FileNotFoundException e) {
+			if (e!=null){
+				XmlNode root = invDoc.CreateElement("Inventory"); 
+			}
+		}
+		foreach (Item item in inventory){
+			XmlAttribute name = item.name;
+			XmlAttribute type = item.type;
+			XmlAttribute image = item.image;
+
+
+		}
+	}
+	void Save(){
+		invDoc.Save("Data/inventory.xml");
+	}
+}
---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item {
+	public  string name;
+	public  string type;
+	public  Texture2D image;
+
+
+	public Item (string name,string type, Texture2D image){
+		this.name = name;
+		this.type = type;
+		this.image = image;
+	}
+}
